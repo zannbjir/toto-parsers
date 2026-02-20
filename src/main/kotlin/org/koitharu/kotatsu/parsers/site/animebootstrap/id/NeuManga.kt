67b8501700724ce093cfa1458908c6c46f95ac53
Razan Muhammad Ikhsan <raazan.muhammad.ikhsan@gmail.com>
2026-02-20 16:21:59 +0700

    Update NeuManga parser source URL

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/animebootstrap/id/NeuManga.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/animebootstrap/id/NeuManga.kt
@@ -5,6 +5,6 @@ import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.animebootstrap.AnimeBootstrapParser
 
-@MangaSourceParser("NEUMANGA", "NeuManga.xyz", "id")
+@MangaSourceParser("NEUMANGA", "NeuManga", "id")
 internal class NeuManga(context: MangaLoaderContext) :
-	AnimeBootstrapParser(context, MangaParserSource.NEUMANGA, "neumanga.xyz")
+	AnimeBootstrapParser(context, MangaParserSource.NEUMANGA, "neumanga.id")
