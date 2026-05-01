1ed182bd9885c99db4f477b6588344fa694a47da
Razan Muhammad Ikhsan <raazan.muhammad.ikhsan@gmail.com>
2026-05-01 18:43:10 +0700

    Update CoComic.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/en/CoComic.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/en/CoComic.kt
@@ -3,11 +3,19 @@ package org.koitharu.kotatsu.parsers.site.madara.en
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.ContentType
+import org.koitharu.kotatsu.parsers.model.MangaListFilterCapabilities
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
+import org.koitharu.kotatsu.parsers.model.MangaTag
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
-import org.koitharu.kotatsu.parsers.Broken
 
-@Broken
 @MangaSourceParser("COCOMIC", "CoComic", "en", ContentType.HENTAI)
 internal class CoComic(context: MangaLoaderContext) :
-	MadaraParser(context, MangaParserSource.COCOMIC, "cocomic.co")
+	MadaraParser(context, MangaParserSource.COCOMIC, "cocomic.co") {
+
+	override val filterCapabilities: MangaListFilterCapabilities
+		get() = MangaListFilterCapabilities(
+			isSearchSupported = true,
+		)
+
+	override suspend fun fetchAvailableTags(): Set<MangaTag> = emptySet()
+}
