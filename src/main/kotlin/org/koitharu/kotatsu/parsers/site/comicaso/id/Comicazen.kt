75f92979aa9a8546b216134fce6bfffbb482cc63
Razan Muhammad Ikhsan <raazan.muhammad.ikhsan@gmail.com>
2026-05-01 19:04:56 +0700

    Update Comicazen.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/comicaso/id/Comicazen.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/comicaso/id/Comicazen.kt
@@ -1,12 +1,10 @@
 package org.koitharu.kotatsu.parsers.site.comicaso.id
 
-import org.koitharu.kotatsu.parsers.Broken
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.comicaso.ComicasoParser
 
-@Broken
 @MangaSourceParser("COMICAZEN", "Comicazen", "id")
 internal class Comicazen(context: MangaLoaderContext) :
 	ComicasoParser(context, MangaParserSource.COMICAZEN, "comicazen.com", pageSize = 16)
