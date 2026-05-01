90a5d142fbf33e405346a53120c4815c61be7a4f
Razan Muhammad Ikhsan <raazan.muhammad.ikhsan@gmail.com>
2026-05-01 18:41:25 +0700

    Update KomikLokalParser.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/id/KomikLokalParser.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/id/KomikLokalParser.kt
@@ -2,15 +2,13 @@ package org.koitharu.kotatsu.parsers.site.mangareader.id
 
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
+import org.koitharu.kotatsu.parsers.model.ContentType
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.mangareader.MangaReaderParser
-import org.koitharu.kotatsu.parsers.Broken
-import java.util.*
 
-@Broken
-@MangaSourceParser("KOMIKLOKAL", "KomikLokal", "id")
+@MangaSourceParser("KOMIKLOKAL", "KomikLokal", "id", ContentType.HENTAI)
 internal class KomikLokalParser(context: MangaLoaderContext) :
 	MangaReaderParser(context, MangaParserSource.KOMIKLOKAL, "komikmu.top", pageSize = 20, searchPageSize = 10) {
-	override val datePattern = "MMM d, yyyy"
-	override val sourceLocale: Locale = Locale.ENGLISH
+	override val listUrl = "/komik"
+	override val datePattern = "dd/MM/yyyy"
 }
